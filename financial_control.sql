b19faaf811da51106f417b22174071e42f112054
Franciane Pires <franpp22@gmail.com>
2023-09-21 19:56:45 -0300

    Update financial_control.sql

--- financial_control.sql
+++ financial_control.sql
@@ -33,39 +33,39 @@ VALUES ('Venda', 'Sandalha', 149.90, '2023-09-13');
 
 select * from transactions -- mostrar todas as inserções 
  
-ID	Tipo	Nome	        Valor	Data_de_cadastro
-1	Compra	Calça Jeans	    99.90	2023-09-15
-2	Venda	Camisa	        49.90	2023-09-15
-3	Venda	Tenis	        79.90	2023-09-15
-4	Venda	Tenis	        119.90	2023-09-14
-5	Venda	Sandalha	    149.90	2023-09-13
-6	Venda	Sandalha	    149.90	2023-09-13
+-- ID	Tipo	Nome	        Valor	Data_de_cadastro
+-- 1	Compra	Calça Jeans	99.90	2023-09-15
+-- 2	Venda	Camisa	        49.90	2023-09-15
+-- 3	Venda	Tenis	        79.90	2023-09-15
+-- 4	Venda	Tenis	        119.90	2023-09-14
+-- 5	Venda	Sandalha	149.90	2023-09-13
+-- 6	Venda	Sandalha	149.90	2023-09-13
 
 
 select * from transactions order by Data_de_cadastro; -- mostrar inserções por data 
 
-Id  Tipo    Nome            Valor   Data_de_cadastro
-5   Venda   Sandalha        149.90  2023-09-13
-6   Venda   Sandalha        149.90  2023-09-13
-4   Venda   Tenis           119.90  2023-09-14
-1   Compra  Calça Jeans     99.90   2023-09-15
-2   Venda   Camisa          49.90   2023-09-15
-3   Venda   Tenis           79.90   2023-09-15
+-- Id  Tipo    Nome            Valor   Data_de_cadastro
+-- 5   Venda   Sandalha        149.90  2023-09-13
+-- 6   Venda   Sandalha        149.90  2023-09-13
+-- 4   Venda   Tenis           119.90  2023-09-14
+-- 1   Compra  Calça Jeans     99.90   2023-09-15
+-- 2   Venda   Camisa          49.90   2023-09-15
+-- 3   Venda   Tenis           79.90   2023-09-15
 
 
 SELECT SUM(Valor) AS total FROM transactions -- mostrar valor total 
 
-total
-649.40
+-- total
+-- 649.40
 
 DELETE FROM transactions WHERE id = 1;
 
 select * from  transactions;
 
-Id  Tipo    Nome        Valor   Data_de_cadastro
-2	Venda	Camisa      49.90	2023-09-15
-3	Venda	Tenis       79.90	2023-09-15
-4	Venda	Tenis       119.90	2023-09-14
-5	Venda	Sandalha    149.90	2023-09-13
-6	Venda	Sandalha    149.90	2023-09-13
+-- Id  Tipo    Nome        Valor   Data_de_cadastro
+-- 2	Venda	Camisa      49.90	2023-09-15
+-- 3	Venda	Tenis       79.90	2023-09-15
+-- 4	Venda	Tenis       119.90	2023-09-14
+-- 5	Venda	Sandalha    149.90	2023-09-13
+-- 6	Venda	Sandalha    149.90	2023-09-13
 				
